9fa38e92ef07c67abbde90f192b5efded6bbf7d6
BBranko93 <bane_b80@outlook.com>
2021-02-25 19:48:19 -0700

    Add project files.

---
+++ OwensAbilityScoreTester/AbilityScoreCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OwensAbilityScoreTester
+{
+    class AbilityScoreCalculator
+    {
+        public int RollResult = 14;
+        public double DivideBy = 1.75;
+        public int AddAmount = 2;
+        public int Minimum = 3;
+        public int Score;
+
+        public void CalculateAbilityScore()
+        {
+            // Divide the roll result by the DivideBy field
+            double divide = RollResult / DivideBy;
+
+            // Add AddAmount to the result of the devision
+            int added = AddAmount + (int)divide;
+
+            // If the result is to small, use Minimum
+            if (added < Minimum)
+            {
+                Score = Minimum;
+            }
+            else
+            {
+                Score = added;
+            }
+        }
+    }
+}
